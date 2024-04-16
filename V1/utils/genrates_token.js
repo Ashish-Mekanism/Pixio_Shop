f6b46594ddad9e1518e66aaee6cf3ea7a92eecae
ashu8383 <rajyaguru838@gmail.com>
2024-04-16 14:30:28 +0530

    Till Login API

--- V1/utils/genrates_token.js
+++ V1/utils/genrates_token.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const GenerateAccessToken = (email) => {
-  return jwt.sign({ email }, "Ecom_token", { expiresIn: "1d" });
+const GenerateAccessToken = (_id) => {
+  return jwt.sign({ _id }, "Ecom_token", { expiresIn: "1d" });
 };
 
 export default GenerateAccessToken;
